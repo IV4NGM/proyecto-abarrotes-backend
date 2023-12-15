bcde4a33085f028522ab8e1d3e14819ce759f5db
IvanGM <ivangm_01@hotmail.com>
2023-12-15 13:02:17 -0600

    feat: Parámetros modificados para staging connection

--- knexfile.js
+++ knexfile.js
@@ -18,9 +18,10 @@ module.exports = {
   staging: {
     client: 'postgresql',
     connection: {
-      database: process.env.DB_NAME_DEV,
-      user: process.env.DB_USER_DEV,
-      password: process.env.DB_PASS_DEV
+      host: process.env.DB_HOST_STG,
+      database: process.env.DB_NAME_STG,
+      user: process.env.DB_USER_STG,
+      password: process.env.DB_PASS_STG
     },
     pool: {
       min: 2,
